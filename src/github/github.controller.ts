0618ac5b2bca548e378ce83ebf896005190aa8e1
Jules Savang <jules.savang@epitech.eu>
2022-03-05 20:10:51 +0100

    add github webhook creation

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -7,14 +7,36 @@ import {
   Param,
   Body,
   Req,
+  Headers,
 } from '@nestjs/common';
 import Firebase from '../firebase/firebase';
 import {Token, Id, ActionId} from '../error/error';
+import { GithubWebhookEvents } from '@dev-thought/nestjs-github-webhooks';
+const { Octokit } = require("@octokit/rest");
+
+const { info } = require("firebase-functions/lib/logger");
+
+async function create_webhook_github (owner: string, repo: string, event : string, url : string) {
+    const octokit = new Octokit({ auth: `ghp_u1ydXnop4Czyi3J4076cr3LeARxbV11XZEW` });
+
+    await octokit.request('POST /repos/{owner}/{repo}/hooks', {
+      owner: owner,
+      repo: repo,
+      events: [
+        event
+      ],
+      config: {
+        url: url,
+        content_type: "json"
+      }
+    })
+  }
+
 @Controller('/services/github')
 export class GithubController {
  @Post('subscribe')
   async subscribe(@Req() request: Request, @Body('token') token: string) {
-    if (!token || token === undefined)
+    if (!token || token == undefined)
       return { message: '400 Bad Parameter'}
     const data = {
       github_token: token,
@@ -57,8 +79,27 @@ export class GithubController {
     return { message: 'Unsubscribed to github service' };
   }
 
-  @Post('/action')
-  async createGithubAction(@Body() token: string) {
+
+  @Post('/action/push')
+  async createGithubPushAction(@Body() token: string, @Body() userName: string, @Body() repoName: string) {
+    if (!token || token == undefined)
+      return { message: '400 Bad Parameter'}
+    const data = {
+      token: token,
+    };
+    await Firebase.getInstance()
+      .getDb()
+      .collection('area')
+      .doc('uuid')
+      .collection('actions')
+      .doc()
+      .set(data);
+    
+    create_webhook_github(userName, repoName, "push", "https://europe-west1-area-37a17.cloudfunctions.net/api/services/github/webhook");
+  }
+
+  @Post('/action/pull_request')
+  async createGithubPullRequestAction(@Body() token: string, @Body() userName: string, @Body() repoName: string) {
     if (!token || token === undefined)
       return { message: '400 Bad Parameter'}
     const data = {
@@ -71,9 +112,83 @@ export class GithubController {
       .collection('actions')
       .doc()
       .set(data);
+    
+    create_webhook_github(userName, repoName, "pull_request", "https://europe-west1-area-37a17.cloudfunctions.net/api/services/github/webhook");
   }
 
-  @Post('/reaction')
+  @Post('/action/issues')
+  async createGithubIssuesAction(@Body() token: string, @Body() userName: string, @Body() repoName: string) {
+    if (!token || token === undefined)
+      return { message: '400 Bad Parameter'}
+    const data = {
+      token: token,
+    };
+    await Firebase.getInstance()
+      .getDb()
+      .collection('area')
+      .doc('uuid')
+      .collection('actions')
+      .doc()
+      .set(data);
+    
+    create_webhook_github(userName, repoName, "issues", "https://europe-west1-area-37a17.cloudfunctions.net/api/services/github/webhook");
+  }
+
+  @Post('/action/issue_comment')
+  async createGithubIssueCommentAction(@Body() token: string, @Body() userName: string, @Body() repoName: string) {
+    if (!token || token == undefined)
+      return { message: '400 Bad Parameter'}
+    const data = {
+      token: token,
+    };
+    await Firebase.getInstance()
+      .getDb()
+      .collection('area')
+      .doc('uuid')
+      .collection('actions')
+      .doc()
+      .set(data);
+    
+    create_webhook_github(userName, repoName, "issue_comment", "https://europe-west1-area-37a17.cloudfunctions.net/api/services/github/webhook");
+  }
+
+  @Post('/action/label')
+  async createGithubLabelAction(@Body() token: string, @Body() userName: string, @Body() repoName: string) {
+    if (!token || token == undefined)
+      return { message: '400 Bad Parameter'}
+    const data = {
+      token: token,
+    };
+    await Firebase.getInstance()
+      .getDb()
+      .collection('area')
+      .doc('uuid')
+      .collection('actions')
+      .doc()
+      .set(data);
+    
+    create_webhook_github(userName, repoName, "label", "https://europe-west1-area-37a17.cloudfunctions.net/api/services/github/webhook");
+  }
+
+  @Post('/action/milestone')
+  async createGithubMilestoneAction(@Body() token: string, @Body() userName: string, @Body() repoName: string) {
+    if (!token || token == undefined)
+      return { message: '400 Bad Parameter'}
+    const data = {
+      token: token,
+    };
+    await Firebase.getInstance()
+      .getDb()
+      .collection('area')
+      .doc('uuid')
+      .collection('actions')
+      .doc()
+      .set(data);
+    
+    create_webhook_github(userName, repoName, "milestone", "https://europe-west1-area-37a17.cloudfunctions.net/api/services/github/webhook");
+  }
+
+@Post('/reaction')
   async createGithubReaction(
     @Body('id') id: Id,
     @Body('actionId') actionId: ActionId,
@@ -100,8 +215,38 @@ export class GithubController {
       .set(data);
   }
 
+  @GithubWebhookEvents(['push', 'pull_request', 'issues', 'issue_comment', 'label'])
   @Post('/webhook')
-  async React(@Request() request) {
-    console.log(request);
+  async ReactGithubWebhook(@Headers('X-GitHub-Event') header : any, @Body() payload : any) {
+    switch (header) {
+      case 'push' : {
+        info("New push", {key1 : header});
+        break;
+      }
+      case 'pull_request' : {
+        info("New PR", {key1 : header});
+        break;
+      }
+      case 'issues' : {
+        info("New issue", {key1 : header});
+        break;
+      }
+      case 'issue_comment' : {
+        info("New issue commment", {key1 : header});
+        break;
+      }
+      case 'label' : {
+        info("New label 5", {key1 : header});
+        break;
+      }
+      case 'milestone' : {
+        info("New milestone", {key1 : header});
+        break;
+      }
+    }
+    //console.log(payload);
+    //console.log("Something have been push");
+    //functions.logger.console.log("Something have been push");
+    //info("Something new happend on repo", {key1 : header});
   }
 }
