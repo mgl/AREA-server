084cf42f2b8ce5b96580768f674723cb360399c7
Romain-GABET <62942880+Romain-GABET@users.noreply.github.com>
2022-03-16 12:58:21 +0100

    Update about.service.ts

--- src/about/about.service.ts
+++ src/about/about.service.ts
@@ -2,106 +2,116 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AboutService {
-  getAbout(req): object {
-    return {
-      client: { host: req.ip },
-      server: {
-        current_time: Math.round(Date.now() / 1000),
-        services: [
-          {
-            name: 'teams',
-            actions: [
-              {
-                name: 'tagged_in_msg',
-                description: 'User has been tagged in a message',
-              },
-            ],
-          },
-          {
-            name: 'twitter',
-            actions: [
-              {
-                name: 'new_message_inbox',
-                description: 'User has received a private message',
-              },
-              {
-                name: 'tagged_in_tweet',
-                description:
-                  'User has been tagged by a user with more than N followers',
-              },
-              {
-                name: 'user_is_retweeted',
-                description:
-                  'User has been retweeted by an account with more than N followers',
-              },
-            ],
-            reactions: [
-              { name: 'follow_user', description: 'Follows a user on Twitter' },
-              { name: 'post_tweet', description: 'Posts a new tweet' },
-            ],
-          },
-          {
-            name: 'google_calendar',
-            actions: [
-              {
-                name: 'new_meeting_created',
-                description: 'A new meeting is created in calendar',
-              },
-            ],
-            reactions: [
-              {
-                name: 'create_calendar_event',
-                description: 'Creates a new calendar event',
-              },
-            ],
-          },
-          {
-            name: 'onedrive',
-            actions: [
-              {
-                name: 'new_meeting_created',
-                description: 'A new meeting is created in calendar',
-              },
-              {
-                name: 'new_todo_task',
-                description: 'New todo task has been assigned to user',
-              },
-            ],
-            reactions: [
-              { name: 'create_todo_task', description: 'Adds a new todo task' },
-              {
-                name: 'create_calendar_event',
-                description: 'Creates a new calendar event',
-              },
-            ],
-          },
-          {
-            name: 'github',
-            actions: [
-              {
-                name: 'actions_finished',
-                description: 'A Github actions has finished',
-              },
-              {
-                name: 'issue_closed',
-                description: 'A Github issue has been closed',
-              },
-              {
-                name: 'issue_created',
-                description: 'A Github issue has been created',
-              },
-              {
-                name: 'repo_starred',
-                description: 'A Github repo has been created',
-              },
-              {
-                name: 'milestone_created',
-                description: 'A Github milestone has been created',
-              },
-            ],
-          },
-        ],
-      },
-    };
-  }
+    getAbout(req): object {
+        return {
+            client: { host: req.ip },
+            server: {
+                current_time: Math.round(Date.now() / 1000),
+                services: [
+                    {
+                        name: 'twitter',
+                        reactions: [
+                            { name: 'follow', description: 'Follows a user on Twitter' },
+                            { name: 'tweet', description: 'Posts a new tweet' },
+                            { name: 'retweet', description: 'Retweet a tweet' },
+                            { name: 'like', description: 'Like a tweet' },
+                        ],
+                    },
+                    {
+                        name: 'mail',
+                        reactions: [
+                            {
+                                name: 'mail',
+                                description: 'Send a mail to a specific amil adress',
+                            },
+                        ],
+                    },
+                    {
+                        name: 'Discord',
+                        reactions: [
+                            {
+                                name: 'message',
+                                description: 'Send a message on a specific channel from a specific server',
+                            },
+                        ],
+                    },
+                    {
+                        name: 'github',
+                        actions: [
+                            {
+                                name: 'pull_request',
+                                description: 'A Github actions has been requested',
+                            },
+                            {
+                                name: 'issue_comment',
+                                description: 'A Github issue has been commented',
+                            },
+                            {
+                                name: 'issue',
+                                description: 'A Github issue has been created',
+                            },
+                            {
+                                name: 'label',
+                                description: 'A Github label has been updated',
+                            },
+                            {
+                                name: 'milestone_created',
+                                description: 'A Github milestone has been created',
+                            },
+                            {
+                                name: 'push',
+                                description: 'A Github milestone has been pushed',
+                            },
+                        ],
+                    },
+                    {
+                        name: 'codebase',
+                        actions: [
+                            {
+                                name: 'Push',
+                                description: 'A Codebase actions has been pushed',
+                            },
+                            {
+                                name: 'merge_request',
+                                description: 'A Github issue has been merged',
+                            },
+                            {
+                                name: 'ticket_creation',
+                                description: 'A Github issue has been created',
+                            },
+                            {
+                                name: 'ticket_update',
+                                description: 'A Github repo has been updated',
+                            },
+                        ],
+                    },
+                    {
+                        name: 'Gitlab',
+                        actions: [
+                            {
+                                name: 'Push',
+                                description: 'A Gitlab actions has been pushed',
+                            },
+                            {
+                                name: 'merge_request',
+                                description: 'A Gitlab issue has been merged',
+                            },
+                            {
+                                name: 'Issue',
+                                description: 'A Gitlab issue has been created',
+                            },
+                            {
+                                name: 'comment',
+                                description: 'A Gitlab repo has been commented',
+                            },
+                            {
+                                name: 'wiki',
+                                description: 'A wiki page has been opened',
+                            },
+                        ],
+                    },
+                ],
+            },
+        };
+    }
 }
